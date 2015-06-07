08c0f145806d0a77e94c82f7ea9a36a9abf3ccff
Eovy Futureworkz <eovy@futureworkz.com>
2015-06-07 11:10:52 +0700

    create view blogs-index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def is_home?
+    true if (controller.controller_name == 'application') and (controller.action_name == 'index')
+  end
 end
---
+++ db/migrate/20150607014919_create_categories_and_blogs.rb
@@ -0,0 +1,17 @@
+class CreateCategoriesAndBlogs < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+    	t.string :title
+
+    	t.timestamps
+    end
+
+    create_table :blogs do |t|
+    	t.string :title
+    	t.string :intro
+    	t.text :content
+
+    	t.timestamps
+    end
+  end
+end
