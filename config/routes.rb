f808cb93315d7db611eb8add12fedd2fb93bd409
Eovy Futureworkz <eovy@futureworkz.com>
2015-06-09 22:26:00 +0700

    Create Services controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :blogs, only: [:index, :show]
   resources :enquiries, only: [:new, :create]
+  resources :services, only: [:index]
 
   # Example resource route with options:
   #   resources :products do
