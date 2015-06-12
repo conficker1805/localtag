f0e8062b52f08c8fb48bc03bcb9b532cde6ceb64
Eovy Futureworkz <eovy@futureworkz.com>
2015-06-13 01:10:15 +0700

    Update smooth scroll jquery

--- app/assets/javascripts/smooth-scroll/smooth-scroll.js
+++ app/assets/javascripts/smooth-scroll/smooth-scroll.js
@@ -1,57 +1,110 @@
-// $(window).on('mousewheel DOMMouseScroll', function (e) { 
-// 	// Set variables
-// 	var height_main_menu = $('header .navbar').height()
 
-// 	// Check direction
-//   var direction = (function () {
-//       var delta = (e.type === 'DOMMouseScroll' ?
-//                    e.originalEvent.detail * -40 :
-//                    e.originalEvent.wheelDelta);
 
-//       return delta > 0 ? true : false;
-//     }());
+$(document).ready(function(){
+	// Set variables
+	var height_main_menu = $('header .navbar').height()
+	var next_node = $('section[data-scroll].next').offset().top - height_main_menu
+	var total = $('section[data-scroll]').size
+	// Initialize flag (keep params to scroll)
+	$('#scrollTo').attr({'data-prev': '0', 'data-curr': '0', 'data-next': '-' + next_node, })
 
-//   // Scrolling
+	// Set event scroll down
+	$('#next').click(function(){
+		next()
+	});
 
-//   // console.log($( '.section-1' ).offset().top)
-//   // console.log($(this).scrollTop())
-  
-//   // var animate_running = $('html,body').is(':animated')
-  
-//   // var target = $( '.section-1' ).offset().top
-//   // if (target > height_main_menu) {
-//   // 	goTop = (target - height_main_menu)*-1
+	// Set event scroll up
+	$('#prev').click(function(){
+		var val_b = '0'
+		$('main, footer').css({
+			'-moz-transform': 		'translate(0, ' + val_b + ')',
+		  '-ms-transform': 			'translate(0, ' + val_b + ')',
+		  '-webkit-transform': 	'translate(0, ' + val_b + ')',
+		  'transform': 					'translate(0, ' + val_b + ')',
+		  '-moz-transform': 		'translate3d(0, ' + val_b + ', 0)',
+		  '-ms-transform': 			'translate3d(0, ' + val_b + ', 0)',
+		  '-webkit-transform': 	'translate3d(0, ' + val_b + ', 0)',
+		  'transform': 					'translate3d(0, ' + val_b + ', 0)',
+		});
+	});
 
-// 	 //  if (direction && !animate_running) {
-// 		//   console.log(goTop)
-// 	 //  	$('body').animate({
-// 	 //        top: goTop
-// 	 //    }, 500, function(){
 
-// 	 //    });
 
-// 	 //  }
 
-//   // }else if (!direction && !animate_running) {
-// 	 //  console.log(goTop)
-//   // 	$('body').animate({
-//   //       top: 0
-//   //   }, 500, function(){
 
+
+
+
+
+
+
+
+	function next() {
+		if (($('section.prev').length > 0) && $('section.next').length <= 0) {
+			// It is last section then scroll to footer
+			var variable = $('#scrollTo').attr('data-next') - $('footer').height() + $(window).height() - $('section').last().height() - height_main_menu
+			$('main, footer').css({ '-moz-transform': 'translate(0, ' + variable + 'px)', '-ms-transform': 'translate(0, ' + variable + 'px)', '-webkit-transform': 'translate(0, ' + variable + 'px)', 'transform': 'translate(0, ' + variable + 'px)', '-moz-transform': 'translate3d(0, ' + variable + 'px, 0)', '-ms-transform': 'translate3d(0, ' + variable + 'px, 0)', '-webkit-transform': 	'translate3d(0, ' + variable + 'px, 0)', 'transform': 'translate3d(0, ' + variable + 'px, 0)',});
+			return
+		}else {
+			$('#next').unbind('click')
+			//$('#prev').unbind('click')
 			
-//   //   });
-//   // }
+			var variable = $('#scrollTo').attr('data-next')
 
-// });
+			$('main, footer').css({ '-moz-transform': 'translate(0, ' + variable + 'px)', '-ms-transform': 'translate(0, ' + variable + 'px)', '-webkit-transform': 'translate(0, ' + variable + 'px)', 'transform': 'translate(0, ' + variable + 'px)', '-moz-transform': 'translate3d(0, ' + variable + 'px, 0)', '-ms-transform': 'translate3d(0, ' + variable + 'px, 0)', '-webkit-transform': 	'translate3d(0, ' + variable + 'px, 0)', 'transform': 'translate3d(0, ' + variable + 'px, 0)',});
 
-// ​$(document).ready(function(){
-//   $('body').bind('mousewheel', function(e){
-//       if(e.originalEvent.wheelDelta /120 > 0) {
-//           console.log('scrolling up !');
-//       }
-//       else{
-//           console.log('scrolling down !');
-//       }
-//   });
-// });
+			setTimeout(function(){ 
+				$('#next').bind('click', next) 
 
+				// Get position of next section
+				$('section').removeClass('prev')
+				var current = $('section[data-scroll].next')
+				current.removeClass('next')
+				current.prev().addClass('prev')
+				if (current.next().length) {
+					current.next().addClass('next')
+
+					// Set flag
+					var new_position = $('#scrollTo').attr('data-next') - $('section[data-scroll].next').offset().top + height_main_menu
+
+					$('#scrollTo').attr('data-next', new_position)
+				}
+			}, 1300)
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	$(window).on('mousewheel DOMMouseScroll', function (e) { 
+	  var scrollable = !$('html,body').is(':animated')
+
+
+		// Check direction
+	  var direction = (function () {
+	      var delta = (e.type === 'DOMMouseScroll' ?
+	                   e.originalEvent.detail * -40 :
+	                   e.originalEvent.wheelDelta);
+
+	      return delta > 0 ? true : false;
+	    }());
+
+
+	  if (direction) {
+	  	$('#next').trigger('click')
+	  }else {
+	  	$('#prev').trigger('click')
+		}
+	  
+	});
+});
