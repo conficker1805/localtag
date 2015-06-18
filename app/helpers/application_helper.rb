451df7492285bec20c17df77b90d228e5ddcc473
Eovy Futureworkz <eovy@futureworkz.com>
2015-06-18 21:31:49 +0700

    Update smooth scroll

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,28 @@
 module ApplicationHelper
   def is_home?
-    true if (controller.controller_name == 'application') and (controller.action_name == 'index')
+    true if (controller_name == 'application') and (action_name == 'index')
   end
 
   def page_title
-  	t("page-title.#{controller.controller_name}.#{controller.action_name}")
+  	t("page-title.#{controller_name}.#{action_name}")
   end
 
   def page_intro
-  	t("page-intro.#{controller.controller_name}.#{controller.action_name}")
+  	t("page-intro.#{controller_name}.#{action_name}")
+  end
+
+  def smooth_scroll?
+  	disable_list = ["blogs/index", "blogs/show"]
+  	"smooth_scroll" unless disable_list.include? "#{controller_name}/#{action_name}"
+  end
+
+  private
+
+  def controller_name
+  	controller.controller_name
+  end
+
+  def action_name
+  	controller.action_name
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :blogs, only: [:index, :show]
+  resources :blogs, except: :destroy
   resources :enquiries, only: [:new, :create]
   resources :services, only: [:index]
   resources :subcribes, only: [:create]
