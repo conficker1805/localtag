cdce3651103d4b703c213e03dfd85265e456c9df
Eovy Futureworkz <eovy@futureworkz.com>
2015-06-21 20:12:14 +0700

    Precompile css

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,13 @@ module Localtag
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w( ckeditor/* )
-    #config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += Ckeditor.assets
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
+    # config.active_record.raise_in_transactional_callbacks = true
+    # config.assets.precompile += Ckeditor.assets
+    # config.assets.precompile += %w( ckeditor/* )
+    # config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
   end
 end
