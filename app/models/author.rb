50ed3b0eac40321d529bc19a51747bf9303fe7f3
Eovy Futureworkz <eovy@futureworkz.com>
2015-06-25 17:31:59 +0700

    Update view dashboard

--- app/models/author.rb
+++ app/models/author.rb
@@ -11,8 +11,8 @@ class Author < ActiveRecord::Base
 
   enumerize :role, in: [:author, :manager], default: :author
 
-  has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "190x190#" } 
-  																#:default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "190x190#" },
+  														:default_url => "avatars/:style/missing.png"
   validates_attachment_size :avatar, :in => 0.megabytes..2.megabytes, message: 'The photo must be less than 2Mb'
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
